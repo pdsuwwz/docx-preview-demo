168e2224e250348a9f5ee1504af79cc7ba1fa8bb
Wisdom <pdsu.wwz@foxmail.com>
2022-05-16 23:10:00 +0800

    🌅 feat: update vite plugins (#5)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { vitePluginCommonjs } from 'vite-plugin-commonjs'
+import commonjs from 'vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vitePluginCommonjs()
+    commonjs()
   ],
   server: {
     port: 4500,
